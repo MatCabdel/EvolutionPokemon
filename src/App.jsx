c3ea3109c11daea728e00892c7c6f200b51eca53
Mathieu Chauveau <chauveau.math@gmail.com>
2023-10-15 17:46:19 +0200

    Premiere version evolution pokemon

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import './App.css'
+import Card from "./Card";
+import { useState } from 'react';
+
+
+const pokemonList = [
+  {
+      name: "Carapuce",
+      level: "Level 5",
+      imgSrc: "./src/media/carapuce.png",
+      attaque: "💧 Charge",
+      attaque2: "💧 Coquille attaque ",
+      force: 5,
+      force1: 15,
+    },
+    {
+      name: "Carabaffe",
+      level: "Level 16",
+      imgSrc:"./src/media/carabaffe.png",
+      attaque: "💧 Vague Geante",
+      attaque2: "💧 Ecume",
+      force: 20,
+      force1: 30,
+    },
+    {
+      name: "Tortank",
+      level: "Level 36",
+      imgSrc:"./src/media/tortank.png",
+      attaque: "💧 Rafale Massive",
+      attaque2: "💧 Hydro Charge",
+      force: 30,
+      force1: 50,
+    },
+  ];
+
+const App = () => {
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleNextPokemon = () => {
+    if (pokemonIndex < pokemonList.length -1)
+      setPokemonIndex(pokemonIndex + 1);
+  }
+  
+  return(
+<div>
+  <h1>Mes Évolutions</h1>
+  <Card pokemon={pokemonList[pokemonIndex]} evolution={handleNextPokemon}/>
+</div>
+  );
+}
+
+
+
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,37 @@
+import './App.scss';
+
+  function Card ({pokemon, evolution}) {
+
+
+
+
+    return (
+      <div className="containerCard">
+        <div className="titleCard">
+        <h3>{pokemon.name}</h3>
+        </div>
+        <img
+        src={pokemon.imgSrc}
+        alt="Carapuce"
+        className="card-img"
+      />
+      <h4>{pokemon.level}</h4>
+      <div className='description'>
+        <ul>
+          <li>
+          <li>{pokemon.attaque}</li>
+          <li>{pokemon.attaque2}</li>
+          </li>
+          <li>
+          <li>{pokemon.force}</li>
+          <li>{pokemon.force1}</li>
+          </li>
+        </ul>
+      </div>
+      <button className='Button1' onClick={evolution}>Evolution</button>
+      </div>
+    
+    )
+  }
+
+  export default Card;
